ece3dd880b5d10d979e45a33139b2912ae01e7e7
Yexson12 <yexsonymalle@gmail.com>
2023-12-13 16:37:42 -0500

    cambio en la ruta

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/BOUTIQUEHELENGIRLS/';
+    public string $baseURL = 'http://localhost/BOUTIQUEHELENGIRL/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
