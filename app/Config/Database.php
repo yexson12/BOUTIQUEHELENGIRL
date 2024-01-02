2c4527e946353e0ad8edf06a235ecd2d1332c760
yexson12 <yexsonymalle@gmail.com>
2024-01-02 14:21:58 -0500

    h

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,8 +27,8 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
+        'username'     => 'root',
+        'password'     => '1234',
         'database'     => '',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
